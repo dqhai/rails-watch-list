6f399a2a55626c5584459857d7afacc1ab273817
Q Hai Dang <haiqd@pm.me>
2022-08-18 15:32:18 +0200

    spec checked

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,21 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  has_many :lists, through: bookmarks
+  # has_many :lists, through: bookmarks
 
-  validates :name, uniqueness: true
+  validates :title, presence: true
+  validates :title, uniqueness: true
   validates :overview, presence: true
+
+  # callbacks
+  # before_destroy :check_for_bookmarkedness
+  # def check_for_bookmarkedness
+  #   status = true
+  #   if self.bookmarks.count > 0
+  #     self.errors[:deletion_status] = 'Cannot delete movies that are currently bookmarked.'
+  #     status = false
+  #   else
+  #     self.errors[:deletion_status] = 'OK.'
+  #   end
+  #   status
+  # end
 end
